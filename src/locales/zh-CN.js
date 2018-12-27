17418438ee1b7a488ec3efce217a87454ca00254
LLH <youyeke@163.com>
2018-12-27 09:34:27 +0800

    更新依赖版本

--- src/locales/zh-CN.js
+++ src/locales/zh-CN.js
@@ -1,5 +1,6 @@
 export default {
   'navBar.lang': '语言',
+  'app.exception.description.403': '您没有权限进行访问',
   'app.exception.description.404': '找不到页面',
   'app.exception.back': '返回主页',
   'menu.account.logout': '退出登录',
